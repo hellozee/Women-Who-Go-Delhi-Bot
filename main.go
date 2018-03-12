ceb8848d8bbdb24530ea99790f3c07560d54375b
hellozee <hellozee@disroot.org>
2018-03-12 14:33:03 +0530

    Organized the code and checked the case for people without username

--- main.go
+++ main.go
@@ -8,7 +8,50 @@ import (
 	"time"
 )
 
+func telegram(m *tb.Message) {
+	b.Send(m.Chat, "https://t.me/womenwhogodelhi")
+}
+
+func facebook(m *tb.Message) {
+	b.Send(m.Chat, "https://www.facebook.com/WomenWhoGoDelhi/")
+}
+
+func twitter(m *tb.Message) {
+	b.Send(m.Chat, "https://twitter.com/womenwhogo_del")
+}
+
+func github(m *tb.Message) {
+	b.Send(m.Chat, "https://github.com/wwgdelhi")
+}
+
+func help(m *tb.Message) {
+	helpMessage := ` Use one of the following commands
+/twitter - to get Women Who Go Delhi Twitter link
+/facebook - to get a link to Women Who Go Delhi Facebook page
+/github - to get a link to Women Who Go Delhi Github page
+/telegram - to get an invite link for Women Who Go Delhi Telegram Group
+
+To contribute to|modify this bot : https://github.com/hellozee/Women-Who-Go-Delhi-Bot
+`
+	b.Send(m.Chat, helpMessage)
+}
+
+func welcome(m *tb.Message) {
+	message := "Welcome "
+
+	if m.UserJoined.Username != "" {
+		message += "@" + m.UserJoined.Username
+	} else {
+		message += m.UserJoined.FirstName
+	}
+
+	message += ", please introduce yourself"
+
+	b.Send(m.Chat, message)
+}
+
 func main() {
+
 	b, err := tb.NewBot(tb.Settings{
 		Token:  os.Getenv("WWGD_KEY"),
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
@@ -19,39 +62,17 @@ func main() {
 		return
 	}
 
-	b.Handle("/telegram", func(m *tb.Message) {
-		b.Send(m.Chat, "https://t.me/womenwhogodelhi")
-		fmt.Println("Got a request")
-	})
+	b.Handle("/telegram", telegram)
 
-	b.Handle("/twitter", func(m *tb.Message) {
-		b.Send(m.Chat, "https://twitter.com/womenwhogo_del")
-	})
+	b.Handle("/twitter", twitter)
 
-	b.Handle("/facebook", func(m *tb.Message) {
-		b.Send(m.Chat, "https://www.facebook.com/WomenWhoGoDelhi/")
-	})
+	b.Handle("/facebook", facebook)
 
-	b.Handle("/github", func(m *tb.Message) {
-		b.Send(m.Chat, "https://github.com/wwgdelhi")
-	})
+	b.Handle("/github", github)
 
-	helpMessage := ` Use one of the following commands
-/twitter - to get Women Who Go Delhi Twitter link
-/facebook - to get a link to Women Who Go Delhi Facebook page
-/github - to get a link to Women Who Go Delhi Github page
-/telegram - to get an invite link for Women Who Go Delhi Telegram Group
+	b.Handle("/help", help)
 
-To contribute to|modify this bot : https://github.com/hellozee/Women-Who-Go-Delhi-Bot
-	`
-
-	b.Handle("/help", func(m *tb.Message) {
-		b.Send(m.Chat, helpMessage)
-	})
-
-	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
-		b.Send(m.Chat, "Welcome @"+m.UserJoined.Username+", Please introduce yourself. ")
-	})
+	b.Handle(tb.OnUserJoined, welcome)
 
 	b.Start()
 
